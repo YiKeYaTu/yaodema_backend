770be469ee92bab3bb37d621665653173e74cff6
YiKeYaTu <liangchen005005@163.com>
2016-08-07 15:00:36 +0800

    asasas

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -28,7 +28,7 @@ export default class extends Base {
             http.host + http.url.slice(0, -1)
         );
 
-        let jsTicket = await wx.getJSSDK(http.host + http);
+        let jsTicket = await wx.getJSSDK(http.host + http.url);
         console.log(jsTicket);
         this.assign('jsTicket', jsTicket);
 
